a9541c5648ecc16d7d6e10391efa26782e1aaa38
Abhinav <118174179+Ab-Abby-19@users.noreply.github.com>
2023-07-03 15:28:06 +0530

    Update excel.py

--- excel.py
+++ excel.py
@@ -2,14 +2,14 @@ import pandas as pd
 import yaml
 
 # Read the YAML file
-with open('/home/abhinav/Abhinav/Ansible/hardwarespecs.yml', 'r') as file:
+with open('destination to path of yml file saved', 'r') as file:
     data = yaml.safe_load(file)
 
 # Convert YAML to pandas DataFrame
 df = pd.json_normalize(data)
 
 # Save DataFrame to Excel
-output_path = '/home/abhinav/Abhinav/Ansible/hardwarespecs.xlsx'
+output_path = 'destination of path of saving excel file'
 df.to_excel(output_path, index=False)
 
 print(f"Excel file saved at: {output_path}")
